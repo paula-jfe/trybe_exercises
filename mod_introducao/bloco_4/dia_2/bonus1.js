e97f5cf73edfdbdc8e97c7cca1f9ae4798c2586c
jpaula <paula.jfe@gmail.com>
2020-08-29 01:18:10 -0300

    Atualizando exercícios dia 2 bloco 4 parte 2

--- mod_introducao/bloco_4/dia_2/bonus1.js
+++ mod_introducao/bloco_4/dia_2/bonus1.js
@@ -1,13 +1,17 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* Se 5 > 9, então posicao = numbers [0], numbers[0+1] = nova posição */
+let posicao = 0;
 
-for (int i = 0; i < numbers.length; i++) { /* contador i - para i menor que elementos da array numbers */
-    for (int j = 0; j < numbers.length - 1; j++) { /* contador j - para j menor que elementos da array numbers e menor que i */
-        if (inteiros[j] > inteiros[j + 1]) { /*  */
-        int inteiroArmazenado = inteiros[j]; /*  */
-        inteiros[j] = inteiros[j + 1]; /*  */
-        inteiros[j + 1] = inteiroArmazenado; /*  */
+for (let i = 0; i < numbers.length; i++) { /* contador i - para i menor que elementos da array numbers */
+    for (let j = 0; j < numbers.length - 1; j++) { /* contador j - para j menor que elementos da array numbers e menor que i */
+        if (numbers[j] > numbers[j + 1]) { /* se numbers na posição j é maior que j numbers na posição j + 1 (posição subsequente)*/
+            posicao = numbers[j]; /* posicao = numbers na posição j */
+            numbers[j] = numbers[j + 1]; /* numbers na posição j = numbers na posição j + 1 (posição subsequente)*/
+            numbers[j + 1] = posicao; /*  */
         }
     }
 }
+
+console.log(numbers);
 
 /* https://www.guj.com.br/t/ordenar-arrays-no-braco-resolvido/53255/4 para ajudar nos exercícios bônus */
