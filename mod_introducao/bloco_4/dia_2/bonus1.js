f44fcb1eb0de75f24064b1bddb278e13f6730b2e
jpaula <paula.jfe@gmail.com>
2020-08-29 01:53:45 -0300

    Atualizando exercícios dia 2 bloco 4 bônus

--- mod_introducao/bloco_4/dia_2/bonus1.js
+++ mod_introducao/bloco_4/dia_2/bonus1.js
@@ -1,17 +1,16 @@
+/* https://www.youtube.com/watch?v=526edG7KsRA&feature=youtu.be para referência*/
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-/* Se 5 > 9, então posicao = numbers [0], numbers[0+1] = nova posição */
 let posicao = 0;
 
-for (let i = 0; i < numbers.length; i++) { /* contador i - para i menor que elementos da array numbers */
-    for (let j = 0; j < numbers.length - 1; j++) { /* contador j - para j menor que elementos da array numbers e menor que i */
-        if (numbers[j] > numbers[j + 1]) { /* se numbers na posição j é maior que j numbers na posição j + 1 (posição subsequente)*/
-            posicao = numbers[j]; /* posicao = numbers na posição j */
-            numbers[j] = numbers[j + 1]; /* numbers na posição j = numbers na posição j + 1 (posição subsequente)*/
-            numbers[j + 1] = posicao; /*  */
+for (let j = 0; j < numbers.length; j +=1) {
+    for (let i = 0; i < numbers.length; i += 1) {
+        if (numbers[i] > numbers [i + 1]) {
+            posicao = numbers[i];
+            numbers[i] = numbers[i + 1];
+            numbers[i + 1] = posicao;
         }
     }
 }
 
 console.log(numbers);
-
-/* https://www.guj.com.br/t/ordenar-arrays-no-braco-resolvido/53255/4 para ajudar nos exercícios bônus */
---
+++ mod_introducao/bloco_4/dia_2/bonus2.js
@@ -0,0 +1,16 @@
+/* https://www.youtube.com/watch?v=526edG7KsRA&feature=youtu.be para referência*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let posicao = 0;
+
+for (let j = 0; j < numbers.length; j +=1) {
+    for (let i = 0; i < numbers.length; i += 1) {
+        if (numbers[i] < numbers [i + 1]) {
+            posicao = numbers[i];
+            numbers[i] = numbers[i + 1];
+            numbers[i + 1] = posicao;
+        }
+    }
+}
+
+console.log(numbers);
