6ed2e02c6cdd3de43ab8c99067991529655450b7
jpaula <paula.jfe@gmail.com>
2020-09-09 12:48:38 -0300

    Atualizando arquivo readme_bloco_5 com exercícios do dia 2

--- mod_introducao/bloco_5/dia_2/scriptexteste1.js
+++ mod_introducao/bloco_5/dia_2/scriptexteste1.js
@@ -1,6 +1,20 @@
-document.querySelector('#elementoOndeVoceEsta').parentNode;
+/* 1 */
+document.querySelector('#elementoOndeVoceEsta');
+/* 2 */
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = "blue";
+/* 3 */
+document.querySelector('#primeiroFilhoDoFilho').innerText = "Você se lembra dos vídeos da aula anterior, como fazer isso? XD"
+/* 4 */
+document.querySelector('#pai').firstChild;
+/* 5 */
+document.querySelector('#elementoOndeVoceEsta').previousSibling;
+/* 6 */
+document.querySelector('#elementoOndeVoceEsta').nextSibling;
+/* 7 */
+document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
+/* 8 */
+document.querySelector('#pai').childNodes[2];
 
-document.getElementById('pai') = yellow;
 
-document.querySelector('#elementoOndeVoceEsta').parentNode;
+
 
