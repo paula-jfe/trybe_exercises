6fa263ebacf69b5093977070341ab53469566f9a
jpaula <paula.jfe@gmail.com>
2020-09-09 00:11:54 -0300

    Adicionando exercícios dia 1 bloco 5

---
+++ mod_introducao/bloco_5/dia_1/scriptexteste2.js
@@ -0,0 +1,7 @@
+document.querySelector("#page-title").innerText = "Interestellar";
+
+document.querySelector("#paragraph").innerText = "6 de novembro de 2014 no Brasil";
+
+document.querySelector("#second-paragraph").innerText = "Interstellar (no Brasil, Interestelar) é um filme anglo-americano de ficção científica dirigido por Christopher Nolan e estrelado por Matthew McConaughey, Anne Hathaway, Jessica Chastain, Bill Irwin, Mackenzie Foy, Matt Damon, John Lithgow e Michael Caine. Ele conta a história de uma equipe de astronautas que viaja através de um buraco de minhoca à procura de um novo lar para a humanidade. Os irmãos Christopher e Jonathan Nolan escreveram o filme unindo ideias do primeiro com um roteiro que o segundo havia escrito em 2007. Nolan foi o produtor junto com sua esposa Emma Thomas e com Lynda Obst. O físico teórico Kip Thorne, cujo trabalho inspirou o filme, trabalhou como consultor científico e como produtor executivo.";
+
+document.querySelector("#subtitle").innerText = "O fim da Terra não será o fim de nós!";
