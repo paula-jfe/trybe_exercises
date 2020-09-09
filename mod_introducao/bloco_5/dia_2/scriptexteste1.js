7c5184b652f898b7ca008dd0eba041c164b99c1b
jpaula <paula.jfe@gmail.com>
2020-09-09 13:11:19 -0300

    scriptexteste1 feito

--- mod_introducao/bloco_5/dia_2/scriptexteste1.js
+++ mod_introducao/bloco_5/dia_2/scriptexteste1.js
@@ -5,15 +5,16 @@ document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = "blue";
 /* 3 */
 document.querySelector('#primeiroFilhoDoFilho').innerText = "Você se lembra dos vídeos da aula anterior, como fazer isso? XD"
 /* 4 */
-document.querySelector('#pai').firstChild;
+document.querySelector('#pai').firstElementChild;
 /* 5 */
-document.querySelector('#elementoOndeVoceEsta').previousSibling;
+document.querySelector('#elementoOndeVoceEsta').previousElementSibling;
 /* 6 */
 document.querySelector('#elementoOndeVoceEsta').nextSibling;
 /* 7 */
 document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
 /* 8 */
-document.querySelector('#pai').childNodes[2];
+/* document.querySelector('#pai').childNodes[2]; */
+document.querySelector('#pai').children[2];
 
 
 
