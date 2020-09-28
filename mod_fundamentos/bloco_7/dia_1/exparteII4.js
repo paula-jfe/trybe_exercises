d82d428fef147f323290a7365cd76c751eca3be4
jpaula <paula.jfe@gmail.com>
2020-09-28 18:02:56 -0300

    Exercícios parteII 4 ok

--- mod_fundamentos/bloco_7/dia_1/exparteII4.js
+++ mod_fundamentos/bloco_7/dia_1/exparteII4.js
@@ -1,17 +1,16 @@
 const arraySkills = ['Javascript', 'CSS', 'HTML', 'Análise de Dados', 'Black belt'];
 const ordArray = arraySkills.sort();
 const newArray = ordArray.join(', ');
-console.log(newArray);
 
-function partFour1(stringInput, parameter) {
+const partFour1 = (stringInput, parameter) => {
   const stringOutput = stringInput.replace('x', parameter);
   return stringOutput;
 }
 
-function partFour2(stringOutput) {
-const result = `${stringOutput} Minhas cinco principais skills são: ${newArray}`;
-return result;
+const partFour2 = stringOutput => {
+  const result = `${stringOutput} Minhas cinco principais skills são: ${newArray}`;
+  return result;
 }
 
-console.log(partFour1('Tryber x aqui!', 'Jessica'));
-console.log(partFour1('Amém'));
+const result = partFour2(partFour1('Tryber x aqui!', 'Jessica'));
+console.log(result);
