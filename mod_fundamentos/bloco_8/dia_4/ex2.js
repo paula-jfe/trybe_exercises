963ceedcf5ab494ad2411a1d658278977d56d25f
jpaula <paula.jfe@gmail.com>
2020-10-09 00:21:23 -0300

    Exercicios dia 8.4

---
+++ mod_fundamentos/bloco_8/dia_4/ex2.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+
+function sum(...parameters) {
+/*   let summation = 0;
+  for (index of parameters) {
+    summation += index;
+  }
+  return summation; */
+  if (parameters === null) {
+    return 0;
+  } else {
+    return parameters.reduce((acc, next) => {
+      return acc + next;
+    }, 0); 
+  }
+};
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
