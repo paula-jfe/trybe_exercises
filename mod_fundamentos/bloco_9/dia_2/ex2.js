5d7058c17a6f340b30b82fa7ce2101afffb59563
paula-jfe <paula.jfe@gmail.com>
2020-10-26 13:58:24 -0300

    Exercicios 3, 4 e 5

--- mod_fundamentos/bloco_9/dia_2/ex2.js
+++ mod_fundamentos/bloco_9/dia_2/ex2.js
@@ -1,4 +1,3 @@
-
 const newArray = [];
 
 function populateArray() {
---
+++ mod_fundamentos/bloco_9/dia_2/ex5.js
@@ -0,0 +1,24 @@
+const newArray = [];
+const resolveArray = [2, 3, 5, 10];
+
+function populateArray() {
+  for (index = 0; index < 10; index += 1) {
+    const newNumber = (Math.floor(Math.random() * 50) + 1);
+    newArray.push(Math.pow(newNumber, 2));
+  }
+  return newArray;
+}
+
+populateArray();
+
+const sum = newArray.reduce((acc, nextValue) => {
+ return acc + nextValue;
+}, 0)
+
+const promiseTest = new Promise((resolve, reject) => {
+  (sum < 8000) ? resolve(sum) : reject()
+})
+
+.then((sum) => resolveArray.map(item => sum / item))
+.then((array) => console.log(array.reduce((acc, nextNumber) => acc + nextNumber)))
+.catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
