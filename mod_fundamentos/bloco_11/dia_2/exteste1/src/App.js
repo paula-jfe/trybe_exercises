8604190bcce71a07ca646e19425392d7b3cef088
paula-jfe <paula.jfe@gmail.com>
2020-11-10 23:17:26 -0300

    Exercicios de teste dia 11.2

---
+++ mod_fundamentos/bloco_11/dia_2/exteste1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Greeting from './Greetings';
+
+function App() {
+  return (
+    <div>
+      <Greeting name="Samuel" lastName="Silva" />
+    </div>
+  );
+}
+
+export default App;
